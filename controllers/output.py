926794777a6b41f031394c15e2b93d1f4003e7f9
Noam Blanks <noamblanks@gmail.com>
2021-09-16 12:45:34 -0400

    pycache dirs removed maybe?

--- controllers/output.py
+++ controllers/output.py
@@ -1,21 +1,51 @@
-from flask import jsonify, request, json, Blueprint
+from flask import jsonify, request, json, Blueprint, g
 from app import db
 
 from models.output import Output
+from models.matrix import Matrix
 from controllers.utilities import key_check
 
 output_bp = Blueprint('output', __name__)
 
+@output_bp.url_value_preprocessor
+def initial_key_check(endpoints, values):
+    api_key = values.pop('api_key')
+    g.user = key_check(api_key)
+    if g.user == None:
+        return jsonify({'status': 401, 'message': 'No such key.'})
+
 
 def output_serializer(output):
-    pass
+    return {
+        "id": output.id,
+        "user_id": output.user_id,
+        "matrix_id": output.matrix_id,
+        "output_title": output.output_title,
+        "generated": output.generated,
+        "created": output.created,
+        "udpated": output.updated
+    }
 
-@output_bp.route('/output/<api_key>', methods=["GET, POST"])
-def get_matrices(api_key):
-    user = key_check(api_key)
-    pass
+@output_bp.route('/output', methods=["GET"])
+def index_outputs():
+    return jsonify([*map(output_serializer, Output.query.filter_by(user_id=g.user.id))])
 
-@output_bp.route('/output/<api_key>/<id>', methods=["GET, PUT, DELETE"])
+@output_bp.route('/matrix/<matrix_id>/output', methods=["POST"])
+def create_output(matrix_id):
+    data = json.loads(request.data)
+    matrix = Matrix.query.get(matrix_id)
+    if matrix.user_id != g.user.id:
+        return jsonify({"status": 400, "message": f'Matrix {matrix_id} does not exist or is not authorized for access by this key'})
+    output = Output(
+        user_id = g.user.id,
+        matrix_id = matrix_id,
+        sample_title = data['sample_title'],
+        generated = some_fucntion(matrix.matrix)
+    )
+
+    return jsonify([*map(output_serializer, Output.query.filter_by(user_id=g.user.id))])
+
+@output_bp.route('/output/<id>', methods=["GET, PUT, DELETE"])
 def single_output(api_key, id):
-    user = key_check(api_key)
+    
     pass
--- controllers/utilities.py
+++ controllers/utilities.py
@@ -1,3 +1,5 @@
+import random
+from flask import g
 from collections import defaultdict
 from models.user import User
 
@@ -18,16 +20,49 @@ def n_gram_er(training_data, n:int=1, gram:str='word'):
             p_matrix[n_gram].append(gram_list[i+n])
     return p_matrix
 
+def text_generator(p_matrix, n:int, gram:str, finished_length:int, first_gram:str=None):
+    if first_gram in p_matrix.keys():
+        chain = first_gram
+    ## TODO: error catch for 'first_gram' not in matrix keys
+    else:
+        chain = random.choice(list(p_matrix.keys()))
+    g.last_gram = chain
+    while len(chain) < finished_length:
+        g.next_gram = random.choice(p_matrix[g.last_gram])
+        chain = chain + g.next_gram
+
+
+        pass
+
+    pass
+
+    # if gram_type == "char":
+    #     while len(output) <= length:
+    #         next_letter = random.choice(dictionary[output[-(len(start)):]])
+    #         output += next_letter
+
+    # if gram_type == "word":
+    #     while len(output) <= length:
+    #         last_gram = output.split(" ")[-1]
+    #         next_gram = random.choice(dictionary[last_gram])
+    #         output += " " + next_gram
+
+    # return(output)
+
+
+
 def mini_sample_serializer(sample):
     return {
         'sample_id': sample.id,
         'sample_title': sample.sample_title
     }
+
 def mini_matrix_serializer(matrix):
     return {
         'matrix_id': matrix.id,
         'matrix_title': matrix.matrix_title
     }
+
 def mini_output_serializer(output):
     return {
         'output_id': output.id,
